66e3d8ddf5258b02cfc3569a5250ff76c8383f5d
H1k1k0m0r1 <112574993+h1k1k0m0r1@users.noreply.github.com>
2022-12-08 16:04:56 -0300

    Add files via upload

---
+++ TUDO/JS copy.js
@@ -0,0 +1,187 @@
+
+function portugues(){
+  var nopt1 = parseFloat(document.getElementById("nopt1").value);
+  var nopt2 = parseFloat(document.getElementById("nopt2").value);
+  var nopt3 = parseFloat(document.getElementById("nopt3").value);
+
+  var mediapt13 = (nopt1 + nopt2 + nopt3)/3 ;
+
+  if(mediapt13 >= 70)
+  if(mediapt13 >= 90)
+   if(mediapt13==69)
+  alert("Uau! Aprovado");
+   else
+    alert("parabens êxito na matéria!");
+   else
+    alert("Parabens, aprovado! Media "+mediapt13);
+
+  else
+   alert("Reprovado na matéria")
+
+ };
+
+
+ function matematica(){
+  var nota1 = parseFloat(document.getElementById("nota1").value);
+  var nota2 = parseFloat(document.getElementById("nota2").value);
+  var nota3 = parseFloat(document.getElementById("nota3").value);
+
+  var mediamat = (nota1 + nota2 + nota3)/3 ;
+
+  if(mediamat >= 70)
+  if(mediamat >= 90)
+   if(mediamat==69)
+  alert("Uau! Aprovado");
+  else
+    alert("parabens êxito na matéria!");
+   else
+    alert("Parabens, aprovado! Media "+mediamat);
+  else
+   alert("Reprovado na matéria")
+
+ };
+
+
+ function quimica(){
+  var notquim1 = parseFloat(document.getElementById("notquim1").value);
+  var notquim2 = parseFloat(document.getElementById("notquim2").value);
+  var notquim3 = parseFloat(document.getElementById("notquim3").value);
+
+  var mediaquimica = (notquim1 + notquim2 + notquim3)/3 ;
+
+  if(mediaquimica >= 70)
+  if(mediaquimica >= 90)
+   if(mediaquimica==69)
+  alert("Uau! Aprovado");
+  else
+    alert("parabens êxito na matéria!");
+   else
+    alert("Parabens, aprovado! Media "+mediaquimica);
+  else
+   alert("Reprovado na matéria")
+
+ };
+
+ function biologia(){
+  var nota_bio1 = parseFloat(document.getElementById("nota_bio1").value);
+  var nota_bio2 = parseFloat(document.getElementById("nota_bio2").value);
+  var nota_bio3 = parseFloat(document.getElementById("nota_bio3").value);
+
+  var mediabiol = (nota_bio1 + nota_bio2 + nota_bio3)/3 ;
+
+  if(mediabiol >= 70)
+  if(mediabiol >= 90)
+   if(mediabiol==69)
+  alert("Uau! Aprovado");
+  else
+    alert("parabens êxito na matéria!");
+   else
+    alert("Parabens, aprovado! Media "+mediabiol);
+  else
+   alert("Reprovado na matéria")
+
+ };
+
+
+
+ function geografia(){
+  var nota_geografia1 = parseFloat(document.getElementById("nota_geografia1").value);
+  var nota_geografia2 = parseFloat(document.getElementById("nota_geografia2").value);
+  var nota_geografia3 = parseFloat(document.getElementById("nota_geografia3").value);
+
+  var mediageo = (nota_geografia1 + nota_geografia2 + nota_geografia3)/3 ;
+
+  if(mediageo >= 70)
+  if(mediageo >= 90)
+   if(mediageo==69)
+  alert("Uau! Aprovado");
+  else
+    alert("parabens êxito na matéria!");
+   else
+    alert("Parabens, aprovado! Media "+mediageo);
+  else
+   alert("Reprovado na matéria")
+
+ };
+
+ function historia(){
+  var nota_historia1 = parseFloat(document.getElementById("nota_historia1").value);
+  var nota_historia2 = parseFloat(document.getElementById("nota_historia2").value);
+  var nota_historia3 = parseFloat(document.getElementById("nota_historia3").value);
+
+  var mediahist = (nota_historia1 + nota_historia2 + nota_historia3)/3 ;
+
+  if(mediahist >= 70)
+  if(mediahist >= 90)
+   if(mediahist==69)
+  alert("Uau! Aprovado");
+  else
+    alert("parabens êxito na matéria!");
+   else
+    alert("Parabens, aprovado! Media "+mediahist);
+  else
+   alert("Reprovado na matéria")
+
+ };
+
+ function filosofia(){
+  var nota_filosofia1 = parseFloat(document.getElementById("nota_filosofia1").value);
+  var nota_filosofia2 = parseFloat(document.getElementById("nota_filosofia2").value);
+  var nota_filosofia3 = parseFloat(document.getElementById("nota_filosofia3").value);
+
+  var mediafilo = (nota_filosofia1 + nota_filosofia2 + nota_filosofia3)/3 ;
+
+  if(mediafilo >= 70)
+  if(mediafilo >= 90)
+   if(mediafilo==69)
+  alert("Uau! Aprovado");
+  else
+    alert("parabens êxito na matéria!");
+   else
+    alert("Parabens, aprovado! Media "+mediafilo);
+  else
+   alert("Reprovado na matéria")
+
+ };
+
+ function espanhol(){
+  var nota_espanhol1 = parseFloat(document.getElementById("nota_espanhol1").value);
+  var nota_espanhol2 = parseFloat(document.getElementById("nota_espanhol2").value);
+  var nota_espanhol3 = parseFloat(document.getElementById("nota_espanhol3").value);
+
+  var mediaespanha = (nota_espanhol1 + nota_espanhol2 + nota_espanhol3)/3 ;
+
+  if(mediaespanha >= 70)
+  if(mediaespanha >= 90)
+   if(mediaespanha==69)
+  alert("Uau! Aprovado");
+  else
+    alert("parabens êxito na matéria!");
+   else
+    alert("Parabens, aprovado! Media "+mediaespanha);
+  else
+   alert("Reprovado na matéria")
+
+ };
+
+
+
+ function Edfisics(){
+  var nota_Edufisica1 = parseFloat(document.getElementById("nota_Edufisica1").value);
+  var nota_Edufisica2 = parseFloat(document.getElementById("nota_Edufisica2").value);
+  var nota_Edufisica3 = parseFloat(document.getElementById("nota_Edufisica3").value);
+
+  var mediaeducação = (nota_Edufisica1 + nota_Edufisica2 + nota_Edufisica3)/3 ;
+
+  if(mediaeducação >= 70)
+  if(mediaeducação >= 90)
+   if(mediaeducação==69)
+  alert("Uau! Aprovado");
+  else
+    alert("parabens êxito na matéria!");
+   else
+    alert("Parabens, aprovado! Media "+mediaeducação);
+  else
+   alert("Reprovado na matéria!")
+
+ };
